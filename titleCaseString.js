1b70ac18c524742dedef9263c8134f456cb695e1
miasuman <smia34872@gmail.com>
2019-05-31 11:12:35 +0600

    yeah

--- titleCaseString.js
+++ titleCaseString.js
@@ -1,22 +1,9 @@
 function titleCase(str) {
-  return str;
-}
-
-titleCase("I'm a little tea pot");
-
-const toTitleCase = phrase => {
-  return phrase
+  return str
     .toLowerCase()
     .split(" ")
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
-};
+}
 
-let result = toTitleCase("maRy hAd a lIttLe LaMb");
-console.log(result);
-
-const arr = ["blonde", "brunette", "redhead"];
-
-const firstLetterOfEachWord = arr.map(word => word.charAt(0));
-
-console.log(firstLetterOfEachWord);
+console.log(titleCase("he is a fucker"));
